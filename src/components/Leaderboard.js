d20442176dfc5b9b31396f9d6bfbeb689f266435
Adrien <adrien.biz@gmail.com>
2023-05-11 20:53:45 +0400

    Style leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -10,26 +10,29 @@ function Leaderboard() {
   }, []);
 
   return (
-    <Table>
-      {scores
-        .sort((a, b) => a.score - b.score)
-        .map((score, index) => (
-          <tr
-            key={index}
-            className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100"
-          >
-            <td className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900">
-              {index}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-              {score.name}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm font-light text-gray-900">
-              {score.score}
-            </td>
-          </tr>
-        ))}
-    </Table>
+    <div>
+      <div className="flex justify-center pb-4 pt-8 text-4xl">Leaderboard</div>
+      <Table>
+        {scores
+          .sort((a, b) => a.score - b.score)
+          .map((score, index) => (
+            <tr
+              key={index}
+              className="border-b bg-white transition duration-300 ease-in-out hover:border hover:border-gray-200 hover:bg-gray-100"
+            >
+              <td className="whitespace-nowrap px-14 py-4 text-sm font-medium text-gray-900">
+                {index}
+              </td>
+              <td className="whitespace-nowrap px-14 py-4 text-sm font-light text-gray-900">
+                {score.name}
+              </td>
+              <td className="whitespace-nowrap px-14 py-4 text-sm font-light text-gray-900">
+                {score.score}
+              </td>
+            </tr>
+          ))}
+      </Table>
+    </div>
   );
 }
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,33 @@
 function Table({ children }) {
   return (
-    <div className="flex flex-col pt-8">
+    <div className="flex flex-col">
       <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-80">
           <div className="overflow-hidden">
             <table className="min-w-full">
               <thead className="border-b bg-gray-200">
                 <tr>
                   <th
                     scope="col"
-                    className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+                    className="px-14 py-4 text-left text-sm font-medium text-gray-900"
                   >
                     Rank
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+                    className="px-14 py-4 text-left text-sm font-medium text-gray-900"
                   >
                     Username
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-4 text-left text-sm font-medium text-gray-900"
+                    className="px-14 py-4 text-left text-sm font-medium text-gray-900"
                   >
                     Time
                   </th>
                 </tr>
               </thead>
-              <tbody>{children}</tbody>
+              <tbody clas>{children}</tbody>
             </table>
           </div>
         </div>
